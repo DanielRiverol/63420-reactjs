21049ef74f5333a5eed693318895828fbf7a2cef
Daniel Riverol <dannyriverol@hotmail.com>
2025-01-18 11:16:29 -0300

    clase_5

--- clase_5/parte_1/src/App.jsx
+++ clase_5/parte_1/src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      
-        <Home/>
-     
+      <Navbar />
+      <Home />
     </>
   );
 }
---
+++ clase_5/parte_1/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+  return (
+    <nav className='navbar'>
+      <ul>
+        <li>
+          <a href='/'>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href='/categories'>
+            Categories
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
